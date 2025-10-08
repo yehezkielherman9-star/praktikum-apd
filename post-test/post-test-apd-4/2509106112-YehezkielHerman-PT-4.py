fa32adf616f6234e974122e7988f2af738723d20
yehezkielherman9 <yehezkielherman9@gmail.com>
2025-10-08 21:39:23 +0800

    pt4

--- post-test/post-test-apd-4/2509106112-YehezkielHerman-PT-4.py
+++ post-test/post-test-apd-4/2509106112-YehezkielHerman-PT-4.py
@@ -1,31 +1,31 @@
-# print ("1. misi pertama yaitu tes konsentrasi")
+print ("1. misi pertama yaitu tes konsentrasi")
 
-# stamina = int(input("masukin stamina kalian berdasarkan dengan digit terakhir nim = "))
+stamina = int(input("masukin stamina kalian berdasarkan dengan digit terakhir nim = "))
 
-# chakra =0
+chakra =0
 
-# while chakra < 200 and stamina > 0:
-#         chakra += 5
-#         stamina -= 3
-#         if stamina < 0:
-#             stamina = 0
+while chakra < 200 and stamina > 0:
+        chakra += 5
+        stamina -= 3
+        if stamina < 0:
+            stamina = 0
 
-# print(f"total chakra yang sudah terkumpul = {chakra}")
-# print(f"stamina yang tersisa = {stamina}")
+print(f"total chakra yang sudah terkumpul = {chakra}")
+print(f"stamina yang tersisa = {stamina}")
 
-# if chakra == 200:
-#     print("anda berhasil menyempurnakan rasengan")
-# else:
-#     print("stamina anda telah habis!!")
+if chakra == 200:
+    print("anda berhasil menyempurnakan rasengan")
+else:
+    print("stamina anda telah habis!!")
 
-# print("2. misi kedua yaitu infiltrasi")
+print("2. misi kedua yaitu infiltrasi")
 
-# tinggi = int(input("masukkan tinggi menara dengan 2 digit nim terakhir = "))
+tinggi = int(input("masukkan tinggi menara dengan 2 digit nim terakhir = "))
 
-# jumlah_gulungan=0
-# for i in range(1, tinggi + 1, 3):
-#      jumlah_gulungan += 1
-# print(f"jumlah gulungan yang anda dapat = {jumlah_gulungan}")
+jumlah_gulungan=0
+for i in range(1, tinggi + 1, 3):
+     jumlah_gulungan += 1
+print(f"jumlah gulungan yang anda dapat = {jumlah_gulungan}")
 
 print("3. Misi ketiga yaitu penyelidikan")
 
@@ -47,5 +47,6 @@ for k in range (1, jumlah_koridor + 1):
             print("didalam nya berisi perangkap peledak")
             perangkap += 1
 
+print("<<<<<MISI SELESAI>>>>>")
 print(f"jumlah data intelejen yang telah di dapatkan {data_intel}")
 print(f"jumlah perangkap peledak yang sudah dijinakkan {perangkap}")
