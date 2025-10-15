7e5c5d99ab5f91944a15af6d34b0611975ac8a48
yehezkielherman9 <yehezkielherman9@gmail.com>
2025-10-15 08:01:15 +0800

    perubahan

--- post-test/post-test-apd-5/25009106112_YehezkielHerman_PT5.py
+++ post-test/post-test-apd-5/25009106112_YehezkielHerman_PT5.py
@@ -1,4 +1,4 @@
-import os 
+import os
 
 dataepep_bro = [
     ['ADMIN', 'minmin09' , 'ADMIN'],
@@ -8,187 +8,182 @@ dataepep_bro = [
 characterepep_bro = [
     ['Kelly','Deadly Velocity', 'Perempuan'],
     ['Alok', 'Drop The Beat', 'Pria'],
-    ['Hayato', 'Bushido', 'Pria']]
-
-def bersihin():
-    os.system("cls" if os.name == "nt" else "bersihin")
-
-
-def create_charepep():
-    print("\n===== CREATE CHARACTER  =====")
-    nama = input("Nama character : ").strip()
-    skill = input("Skill character: ").strip()
-    gender = input("Gender character: ").strip()
-    
-    if not nama or not skill or not gender:
-        print("mana kok gak di isi")
-        return
-    
-    characterepep_bro.append([nama, skill, gender])
-    print(f"character {nama} berhasil di buat")
-
-def listepep_bro():
-        if not characterepep_bro:
-            print("epep belum memiliki Character")
-            
-        else:
-            print("\n=====  DAFTAR CHARACTER EPEP  =====")
-            print(f"{'No':<4}| {'Nama':<20}| {'Skill':<25}| {'Gender':<10}|")
-            print("-" * 65)
-
-            for i, char in enumerate(characterepep_bro):
-                print(f"{i+1:<4}| {char[0]:<20}| {char[1]:<25}| {char[2]:<10}|")
-                print("-" * 65)
-
-
-def updatechar_epep():
-    listepep_bro()
-    if not characterepep_bro:
-        return
-    nomorchar = input("\npilih nomor berapa character epep yang mau di update: ").strip()
-    
-    if not nomorchar.isdigit():
-        print("pake angka bro")
-        return
-    nomorchar= int(nomorchar) - 1
-
-    if nomorchar < 0 or nomorchar >= len(characterepep_bro):
-        print("angka gak ada!!")
-        return
-    
-    print("===== UPDATE CHARACTER =====")
-    nama = input("Nama baru: ").strip()
-    skill = input("skill baru: ").strip()
-    gender = input("gender baru: ").strip()
-
-    if not nama or not skill or not gender:
-        print("mana kok gak di isi")
-        return
-    
-    characterepep_bro[nomorchar] = ([nama, skill, gender])
-    print("character sudah terbaharui")
-
-def hapuschar_epepbro():
-    listepep_bro()
-    if not characterepep_bro:
-        return
-    
-    print("===== HAPUS CHARACTER =====")
-    nomorchar = input("\npilih nomor berapa character epep yang mau dihapus: ").strip()
-
-    if not nomorchar.isdigit():
-        print("pake angka bro")
-        return
-    nomorchar= int(nomorchar) - 1
-
-    if nomorchar < 0 or nomorchar >= len(characterepep_bro):
-        print("angka gak ada!!")
-        return
-    
-    del characterepep_bro[nomorchar]
-    print("character berhasil dihapus bro")
-
-def regis():
-    bersihin()
-    print("===== REGISTER AKUN =====")
-    username = input("Masukkan username baru anda: ").strip()
-    pw = input("Masukkan password anda: ").strip()
-
-    for userbaru in dataepep_bro:
-        if userbaru[0] == username:
-            print("username sudah ada")
-            return
-        
-    dataepep_bro.append([username, pw, "USER"])
-    print("""registrasi anda berhasil\nsilahkan login""")
-
-def login():
-    bersihin()
-    print("===== LOGIN =====")
-    username = input("Username: ").strip()
-    pw = input("Password: ").strip()
-
-    for user in dataepep_bro:
-        if user[0] == username and user[1] == pw:
-            print(f"Selamat datang, {username}!")
-            return user
-    print("Username atau password salah!")
-    return None
-
-def menu_sangadmin():
-    while True:
-        bersihin()
-        print("===== MENU ADMIN =====")
-        print("1. List Character")
-        print("2. Create Character")
-        print("3. Update Character")
-        print("4. Hapus Character")
-        print("5. Logout")
-        pilih = input("Pilih menu: ").strip()
-
-        if pilih == "1":
-            bersihin()
-            listepep_bro()
-            input("\nTekan Enter untuk kembali...")
-        elif pilih == "2":
-            bersihin()
-            create_charepep()
-            input("\nTekan Enter untuk kembali...")
-        elif pilih == "3":
-            bersihin()
-            updatechar_epep()
-            input("\nTekan Enter untuk kembali...")
-        elif pilih == "4":
-            bersihin()
-            hapuschar_epepbro()
-            input("\nTekan Enter untuk kembali...")
-        elif pilih == "5":
-            print("anda berhasil logout")
-            break
-        else:
-            print("Pilihan tidak ada bro!")
-
-def menu_userbro(username):
-    while True:
-        bersihin()
-        print(f"===== MENU USER <{username}> =====")
-        print("1. List Karakter")
-        print("2. Logout")
-        pilih = input("Pilih menu: ").strip()
-
-        if pilih == "1":
-            bersihin()
-            listepep_bro()
-            input("\nTekan Enter untuk kembali...")
-        elif pilih == "2":
-            print("anda berhasil logout")
-            break
-        else:
-            print("Pilihan tidak ada bro!")
+    ['Hayato', 'Bushido', 'Pria']
+]
 
 while True:
-    bersihin()
+    os.system("cls" if os.name == "nt" else "clear")
     print("===== SISTEM LIST CHARACTER GAME EPEP =====")
     print("1. Login")
     print("2. Register")
     print("3. Keluar")
     pilih = input("Pilih menu: ").strip()
 
-    if pilih == "1":
-        user = login()
-        if user:
-            if user [2] == "ADMIN":
-                menu_sangadmin()    
+    if pilih == "2":
+        os.system("cls" if os.name == "nt" else "clear")
+        print("===== REGISTER AKUN =====")
+        username = input("Masukkan username baru anda: ").strip()
+        pw = input("Masukkan password anda: ").strip()
+
+        sudah_ada = False
+        for userbaru in dataepep_bro:
+            if userbaru[0] == username:
+                sudah_ada = True
+
+        if sudah_ada:
+            print("Usernamenya sudah ada")
+        else:
+            dataepep_bro.append([username, pw, "USER"])
+            print("Registrasi berhasil, silakan login")
+
+        input("\nTekan Enter untuk kembali...")
+
+    elif pilih == "1":
+        os.system("cls" if os.name == "nt" else "clear")
+        print("===== LOGIN =====")
+        username = input("Username: ").strip()
+        pw = input("Password: ").strip()
+
+        user_login = None
+        for user in dataepep_bro:
+            if user[0] == username and user[1] == pw:
+                user_login = user
+
+        if user_login == None:
+            print("Username atau password nya salah")
+            input("\nTekan Enter untuk kembali...")
+        else:
+            print(f"Selamat datang, {username}!")
+            input("\nTekan Enter untuk lanjut...")
+
+            if user_login[2] == "ADMIN":
+                keluar_admin = False
+                while not keluar_admin:
+                    os.system("cls" if os.name == "nt" else "clear")
+                    print("===== MENU ADMIN =====")
+                    print("1. List Character")
+                    print("2. Create Character")
+                    print("3. Update Character")
+                    print("4. Hapus Character")
+                    print("5. Logout")
+                    pilih_admin = input("Pilih menu: ").strip()
+
+                    if pilih_admin == "1":
+                        os.system("cls" if os.name == "nt" else "clear")
+                        if not characterepep_bro:
+                            print("engga ada character nya bro")
+                        else:
+                            print("\n===== DAFTAR CHARACTER EPEP =====")
+                            print(f"{'No':<4}| {'Nama':<20}| {'Skill':<25}| {'Gender':<10}|")
+                            print("-" * 65)
+                            for i, char in enumerate(characterepep_bro):
+                                print(f"{i+1:<4}| {char[0]:<20}| {char[1]:<25}| {char[2]:<10}|")
+                                print("-" * 65)
+                        input("\nTekan Enter untuk kembali...")
+
+                    elif pilih_admin == "2":
+                        os.system("cls" if os.name == "nt" else "clear")
+                        print("\n===== CREATE CHARACTER =====")
+                        nama = input("Nama character: ").strip()
+                        skill = input("Skill character: ").strip()
+                        gender = input("Gender character: ").strip()
+
+                        if not nama or not skill or not gender:
+                            print("Semua data nya harus diisi kocak")
+                        else:
+                            characterepep_bro.append([nama, skill, gender])
+                            print(f"Character {nama} berhasil dibuat")
+                        input("\nTekan Enter untuk kembali...")
+
+                    elif pilih_admin == "3":
+                        os.system("cls" if os.name == "nt" else "clear")
+                        if not characterepep_bro:
+                            print("Belum ada character nya untuk diubah bro!")
+                            input("\nTekan Enter untuk kembali...")
+                        else:
+                            print("===== UPDATE CHARACTER =====")
+                            for i, char in enumerate(characterepep_bro):
+                                print(f"{i+1}. {char[0]} | {char[1]} | {char[2]}")
+
+                            nomorchar = input("\nPilih nomor character nya yang mau diupdate: ").strip()
+                            if not nomorchar.isdigit():
+                                print("pake angka bro")
+                            else:
+                                nomor = int(nomorchar) - 1
+                                if nomor < 0 or nomor >= len(characterepep_bro):
+                                    print("character nya tidak ada")
+                                else:
+                                    nama = input("Nama baru: ").strip()
+                                    skill = input("Skill baru: ").strip()
+                                    gender = input("Gender baru: ").strip()
+                                    if not nama or not skill or not gender:
+                                        print("Semua data nya harus diisi dong")
+                                    else:
+                                        characterepep_bro[nomor] = [nama, skill, gender]
+                                        print("Character epep mu sudah berhasil diupdate")
+                            input("\nTekan Enter untuk kembali...")
+
+                    elif pilih_admin == "4":
+                        os.system("cls" if os.name == "nt" else "clear")
+                        if not characterepep_bro:
+                            print("engga ada character nya untuk dihapus")
+                        else:
+                            print("===== HAPUS CHARACTER =====")
+                            for i, char in enumerate(characterepep_bro):
+                                print(f"{i+1}. {char[0]} | {char[1]} | {char[2]}")
+                            nomorchar = input("\nPilih nomor character nya yang mau dihapus: ").strip()
+                            if not nomorchar.isdigit():
+                                print("pake angka dong")
+                            else:
+                                nomor = int(nomorchar) - 1
+                                if nomor < 0 or nomor >= len(characterepep_bro):
+                                    print("character nya enggak ada")
+                                else:
+                                    del characterepep_bro[nomor]
+                                    print("Character epep mu berhasil dihapus")
+                        input("\nTekan Enter untuk kembali...")
+
+                    elif pilih_admin == "5":
+                        print("kamu berhasil logout")
+                        keluar_admin = True
+                    else:
+                        print("Pilihan mu tidak ada kocak!")
+                        input("\nTekan Enter untuk kembali...")
+
             else:
-                menu_userbro(user[0])
-    elif pilih == "2":
-        regis()
-        input("\nTekan Enter untuk kembali...")
+                keluar_user = False
+                while not keluar_user:
+                    os.system("cls" if os.name == "nt" else "clear")
+                    print(f"===== MENU USER <{username}> =====")
+                    print("1. List character nya")
+                    print("2. Logout")
+                    pilih_user = input("Pilih menu: ").strip()
+
+                    if pilih_user == "1":
+                        os.system("cls" if os.name == "nt" else "clear")
+                        if not characterepep_bro:
+                            print("enggak ada character nya nya loh")
+                        else:
+                            print("\n===== DAFTAR CHARACTER EPEP =====")
+                            print(f"{'No':<4}| {'Nama':<20}| {'Skill':<25}| {'Gender':<10}|")
+                            print("-" * 65)
+                            for i, char in enumerate(characterepep_bro):
+                                print(f"{i+1:<4}| {char[0]:<20}| {char[1]:<25}| {char[2]:<10}|")
+                                print("-" * 65)
+                        input("\nTekan Enter untuk kembali...")
+
+                    elif pilih_user == "2":
+                        print("kamu berhasil logout loh")
+                        keluar_user = True
+                    else:
+                        print("Pilihan gak ada bro")
+                        input("\nTekan Enter untuk kembali...")
+
     elif pilih == "3":
-        bersihin()
-        print("Terima kasih bro karena sudah menggunakan program ini")
+        os.system("cls" if os.name == "nt" else "clear")
+        print("Terima kasih karena sudah menggunakan program ini bro")
         break
+
     else:
-        print("Pilihan tidak ada bro!")
+        print("Pilihan tidak ada kocakk")
         input("\nTekan Enter untuk kembali...")
-    
